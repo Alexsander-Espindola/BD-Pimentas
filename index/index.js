352a4a52be87ed05bdf552e36069b81974c73476
Alexsander Espindola <alexsander81002745@gmail.com>
2022-11-23 17:26:30 -0300

    First commit

---
+++ index/index.js
@@ -0,0 +1,4 @@
+const qntdUnitarios = document.getElementById('qntd-unitarios');
+const custoPorUnidade = document.getElementById('custo-por-unidade');
+
+console.log(qntdUnitarios.inputEncoding, custoPorUnidade);
